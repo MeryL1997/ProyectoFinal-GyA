0ac0afc1ec930bb74fe1f49e84152efeb8172695
MeryL1997 <50187870+MeryL1997@users.noreply.github.com>
2019-08-12 17:46:51 -0500

    prueba

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default class Title extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1> Desarrolo del project</h1>
+                <p>Desarrolo del project</p>
+            </div>
+        );
+    }
+}
